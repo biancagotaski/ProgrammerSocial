90b1b7a840d93d74404d6a22745453dc59964a91
Bianca Gotaski <biancagotaski@outlook.com.br>
2018-03-13 18:42:29 -0300

    Draft initial v0

---
+++ Business/Preferences.cs
@@ -0,0 +1,12 @@
+﻿namespace Business
+{
+    public class Preferences
+    {
+        public string CSharp { get; set; }
+        public string Java { get; set; }
+        public string JavaScript { get; set; }
+        public string Perl { get; set; }
+        public string C { get; set; }
+        public string CPlusPlus { get; set; }
+    }
+}
---
+++ Business/Speciality.cs
@@ -0,0 +1,10 @@
+﻿namespace Business
+{
+    public class Speciality
+    {
+        public string Studant { get; set; }
+        public string Specialist { get; set; }
+        public string Curious { get; set; }
+        public string Beginner { get; set; }
+    }
+}
